fcf94209d3f8fc295c5b0bb9205b94d195bfd376
Eduardo Balsa <ebalsa@ebserver.org>
2026-04-17 10:25:08 +0200

    Disclaim that API key is public

--- youtube/client.go
+++ youtube/client.go
@@ -16,7 +16,7 @@ import (
 
 const (
 	baseURL         = "https://www.youtube.com/youtubei/v1/"
-	apiKey          = "AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8"
+	apiKey          = "AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8" # Public API key
 	clientName      = "WEB"
 	clientVersion   = "2.20260114.08.00"
 	userAgent       = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
